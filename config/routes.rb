1a55d60f0d7ff549fe4539ba7c35f77cc26ae45c
Faiz Authar <faizauthar@gmail.com>
2022-03-28 11:21:02 +0700

    config: routes: Simplified routes for all "Items"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/show'
-  get 'items/new'
-  get 'items/edit'
+  resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
